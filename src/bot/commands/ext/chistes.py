ac6c311ea2472ed7a0376745ea4ab8e07664bb95
Isael Diroche <isaeldiroche00@gmail.com>
2023-06-15 10:28:38 -0400

    fix:

--- src/bot/commands/ext/chistes.py
+++ src/bot/commands/ext/chistes.py
@@ -1,5 +1,13 @@
 # Dependencias necesarias desde el script principal
+from bs4 import BeautifulSoup
+import requests
 from bot import Update, CallbackContext, chistesESP
+
+def get_random_chiste():
+    response = requests.get('http://www.todo-chistes.com/chistes-al-azar')
+    soup = BeautifulSoup(response.text, "html.parser")
+    chiste = soup.find("div", "field-chiste").text
+    return chiste
 
 # Codigo aqui
 def command(update:Update, context:CallbackContext):
